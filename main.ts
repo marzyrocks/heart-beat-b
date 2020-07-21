46e0c954f357c916d21971a5a487dae50bf2d44c
marzyrocks <66854721+marzyrocks@users.noreply.github.com>
2020-07-21 12:37:26 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+let digital_write_pin_P1 = 0
+let digital_write_pin_P2 = 0
 input.onButtonPressed(Button.A, function () {
     basic.showString("HELLO!")
     music.playMelody("E C C F F C C E ", 98)
@@ -24,6 +26,18 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showString("LOVE!")
     music.playMelody("A C5 C5 G G C5 C5 A ", 98)
+})
+basic.forever(function () {
+    digital_write_pin_P1 = 1
+    basic.pause(200)
+    digital_write_pin_P1 += 0
+    basic.pause(100)
+})
+basic.forever(function () {
+    digital_write_pin_P2 = 0
+    basic.pause(200)
+    digital_write_pin_P2 += 1
+    basic.pause(100)
 })
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
